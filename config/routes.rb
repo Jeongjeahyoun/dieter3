e6e90c406de435a64df72ab69833fb2a3ce9a3ba
dongwonkay <dongwon@likelion.org>
2017-07-09 19:46:15 +0000

    로그인 닉네임변경

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' } 
   
   root 'home#index'
   post '/tinymce_assets' => 'tinymce_assets#create'
